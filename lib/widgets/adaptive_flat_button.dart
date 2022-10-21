ba11416ced83107c41ac1e590ae17643b9be08e9
Muhammad Aqeel <44490854+AqeelAaqi@users.noreply.github.com>
2022-10-21 16:13:26 +0500

    Final Comment => Cupertino IOS version added

--- lib/widgets/adaptive_flat_button.dart
+++ lib/widgets/adaptive_flat_button.dart
@@ -4,7 +4,9 @@ import 'package:flutter/cupertino.dart';
 
 class AdaptiveFlatButton extends StatelessWidget {
   final String text;
-  final VoidCallback  handler;
+  final VoidCallback  handler;  //Function  handler
+  //Change your code to accept a VoidCallback instead of Function for the onPressed.
+  // By the way VoidCallback is just shorthand for void Function() so you could also define it as final void Function() onPressed;
 
   AdaptiveFlatButton(this.text, this.handler);
 
